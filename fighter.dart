9c84dbd013c6a07128e1b975e5c8082eefe0d079
David Grammatico <david.lugdunum@gmail.com>
2022-10-10 12:59:02 +0200

    add class fighter

---
+++ fighter.dart
@@ -0,0 +1,24 @@
+import 'dart:math';
+
+class Fighter {
+  int _health = 100;
+  int _strength = 1;
+
+  int get strength => _strength;
+  int get health => _health;
+
+  set strength(int strength) {
+    _strength = max(0, strength);
+  }
+
+  set health(int health) {
+    _health = max(0, health);
+  }
+
+  bool get isAlive => _health > 0;
+
+  isAttacked({required int hitStrength}) {
+    // _health = max(0, _health - hitStrength);
+    health -= hitStrength;
+  }
+}
